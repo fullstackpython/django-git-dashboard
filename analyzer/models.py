28e2f03ef52761434e103bc78aa6453af7aa4115
Matt Makai <matthew.makai@gmail.com>
2017-11-15 08:16:47 -0500

    add basic models for repo and commits

--- analyzer/models.py
+++ analyzer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class GitRepository(models.Model):
+    """Represents a single Git repository and its metadata. Each commit is
+    stored in its own repository.
+    """
+    git_url = models.CharField(max_length=1024)
+    local_location = models.CharField(max_length=1024)
+
+
+class GitCommit(models.Model):
+    """A single Git commit and its metadata."""
+    checksum_hash = models.CharField(max_length=40)
+    author = models.CharField(max_length=256)
+    commit_date = models.DateTimeField()
+
+    def __unicode__(self):
+        return checksum_hash
+
+
